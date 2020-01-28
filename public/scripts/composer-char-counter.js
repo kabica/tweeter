14a9d71514413b30763aa0983b495671429ad641
Kevin A. Bica <kalex.bica@gmail.com>
2020-01-28 01:15:46 +0000

    added limit to character count and key rejection for non alpha-numeric

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,27 @@
+i = 140;
+let rejectArr = [9, 13, 16, 17, 18, 20, 91, 37, 38, 39, 40];
 
+const validInput = function (key) {
+  for (let i = 0; i < rejectArr.length; i++) {
+    if (rejectArr[i] === key) {
+      return false;
+    }
+  }
+  return true;
+}
 
-$(document).ready(function() {
+$(document).ready(function () {
   // --- our code goes here ---
-  console.log('hello');
+  $("textarea").on('keydown', function (key) {
+    if (key.keyCode == 8) {
+      if (i < 140) {
+        $(".counter").text(i += 1);
+      }
+    } else {
+      let status = validInput(key.keyCode);
+      if (status) {
+        $(".counter").text(i -= 1);
+      }
+    }
+  })
 });
