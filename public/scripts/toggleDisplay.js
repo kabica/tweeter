6e17beb1e28649af805ffdfcbce8b6e633d594c6
Kevin A. Bica <kalex.bica@gmail.com>
2020-02-06 22:23:16 +0000

    add comments for error toggle functions

--- public/scripts/toggleDisplay.js
+++ public/scripts/toggleDisplay.js
@@ -1,24 +1,24 @@
+// toggleDisplay.js
+
+// Compose new tweet section will toggle visibility
 const toggleTweet= function() {
   var x = document.getElementById("toggleIcon");
   if (x.style.display === "none") {
     $('.newTweet').slideDown();
     $("textarea").focus();
   } else {
-    // x.style.display = "none";
     $('.newTweet').slideUp();
   }
 }
-
-
+// Error for empty tweet attempts
 const underTweet = function(errorType) {
   $(".showError").text(errorType);
   $('.showError').slideDown();
 }
-
 const underTweetHide = function() {
   $('.showError').slideUp();
 }
-
+// Error for over-char limit tweet attempts
 const overTweet = function(errorType) {
   $(".showError").text(errorType);
   $('.showError').slideDown();
